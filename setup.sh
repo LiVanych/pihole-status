98d7d287ac920feea6e916bd4f3b8e805f02beae
Ben Kolin <bkolin@gmail.com>
2020-09-25 14:38:36 -0700

    Install python dependencies, Adafruit SSD1306 driver, @reboot crontab entry

---
+++ setup.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# Install dependencies:
+sudo apt install -y python3-dev
+sudo apt install -y python3-pip
+sudo apt install -y python-smbus i2c-tools
+sudo apt install -y python3-pil
+sudo apt install -y python3-setuptools
+
+# If gpio pins are going to be used:
+# sudo apt install -y python3-rpi.gpio
+
+# Install the Adafruit SSD1306 OLED driver:
+sudo pip3 install adafruit-circuitpython-ssd1306
+
+# Add a crontab entry to start the OLED updating script on boot:
+sudo bash -c 'echo "@reboot python3 /home/pi/pihole-status/pihole_status.py &" > /etc/cron.d/pihole_status.cron'
