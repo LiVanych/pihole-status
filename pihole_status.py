770299a8735407c61ad7aeb137fbd9de347b58a1
Ben Kolin <bkolin@gmail.com>
2020-09-25 14:39:14 -0700

    Basic stats working but unformatted, handling of pihole webserver down in place

--- pihole_status.py
+++ pihole_status.py
@@ -21,8 +21,6 @@
 # Pi-hole stats extraction based on Matt Hawkins' implementation at: 
 #   https://bitbucket.org/MattHawkinsUK/rpispy-misc/src/master/pihole/
 #
-# This implementation is not interactive - no LEDs or switches are used.
-#
 
 
 # Standard libraries
@@ -37,23 +35,27 @@ from PIL import Image
 from PIL import ImageDraw
 from PIL import ImageFont
 
+# I2C ibraries
+from board import SCL, SDA
+import busio
+
 # Adafruit library for I2C OLED screen
-import Adafruit_SSD1306
+import adafruit_ssd1306
 
 # Screen refresh frequency:
-refresh = 30
+refresh = 5 #30
 
-# 128x64 display with hardware I2C:
-disp = Adafruit_SSD1306.SSD1306_128_64(rst=None)
-# disp = Adafruit_SSD1306.SSD1306_128_64(rst=None, i2c_address=0x3C)
-# disp = Adafruit_SSD1306.SSD1306_128_32(rst=None, i2c_bus=2)
-
-# Initialize library.
-disp.begin()
-
+# Create the I2C interface.
+i2c = busio.I2C(SCL, SDA)
+ 
+# Create the SSD1306 OLED class.
+# The first two parameters are the pixel width and pixel height.  Change these
+# to the right size for your display!
+disp = adafruit_ssd1306.SSD1306_I2C(128, 64, i2c)
+ 
 # Clear display.
-disp.clear()
-disp.display()
+disp.fill(0)
+disp.show()
 
 # Create blank image for drawing.
 # Make sure to create image with mode '1' for 1-bit color.
@@ -66,33 +68,42 @@ draw = ImageDraw.Draw(image)
 
 # Draw some shapes.
 # First define some constants to allow easy resizing of shapes.
-padding = 0
+padding = 0 
 top = padding
 bottom = height-padding
+border = 5
 # Move left to right keeping track of the current x position for drawing shapes.
 x = 0
 
 # Load Truetype font from https://www.dafont.com/bitmap.php
 # VCR OSD Mono by Riciery Leal
-font = ImageFont.truetype('VCR_OSD_MONO_1.001.ttf',15)
-font2 = ImageFont.truetype('VCR_OSD_MONO_1.001.ttf',40)
+#font = ImageFont.truetype('VCR_OSD_MONO_1.001.ttf',15)
+#font2 = ImageFont.truetype('VCR_OSD_MONO_1.001.ttf',40)
 
-#font = ImageFont.load_default()
+font = ImageFont.load_default()
 
 def clear_screen():
   # Draw a black filled box to clear the image.
   draw.rectangle((0,0,width,height), outline=0, fill=0)
 
-clear_screen()
+def highlight_text(text, font):
+  draw.rectangle((0,0,width,height), outline=255, fill=255)
+  draw.rectangle((border, border, width - border - 1, height - border - 1),
+      outline=0,
+      fill=0,
+  )
+  (font_width, font_height) = font.getsize(text)
+  draw.text((width/2 - font_width/2, height/2 - font_height/2) , text,  font=font, fill=255)
 
-# Show Start Script text
-draw.text((x, top), "Start Script",  font=font, fill=255)
+# Display startup text.
+title = "Pi-hole Status"
+highlight_text(title, font)
 disp.image(image)
-disp.display()
+disp.show()
 
-# Default mode, show large percentage
-mode=0
-counter=1
+# Default mode to showing system info first
+mode = 1
+counter = 1
 
 while True:
 
@@ -104,13 +115,18 @@ while True:
       # Pi-hole data
 
       # Get Pi-Hole data from local admin web page
-      r = requests.get("http://localhost/admin/api.php?summary")
+      try:
+        r = requests.get("http://localhost/admin/api.php?summary")
 
-      # Display large Pi-Hole ads blocked percentage
-      draw.text((x, top-2), "%s%%" % r.json()["ads_percentage_today"],  font=font2, fill=255)
-      draw.text((x, top+32), "Ads blocked: %s" % r.json()["ads_blocked_today"], font=font, fill=255)   
-      draw.text((x, top+48), "DNS queries: %s" % r.json()["dns_queries_today"], font=font, fill=255)
- 
+        # Display Pi-hole stats
+        draw.text((x, top), "%s%%" % r.json()["ads_percentage_today"],  font=font, fill=255)
+        draw.text((x, top+32), "Ads blocked: %s" % r.json()["ads_blocked_today"], font=font, fill=255)
+        draw.text((x, top+48), "DNS queries: %s" % r.json()["dns_queries_today"], font=font, fill=255)
+      
+      except requests.exceptions.RequestException as e:  # This is the correct syntax
+        #raise SystemExit(e)
+        highlight_text("Pi-hole is DOWN!!!", font)
+        
       # Toggle
       mode = 1
 
@@ -118,36 +134,40 @@ while True:
       # System data
 
       # Shell scripts for system monitoring from here : https://unix.stackexchange.com/questions/119126/command-to-display-memory-usage-disk-usage-and-cpu-load
+
+      # Note: subprocess.check_output returns the process output with a trailing \n. Use strip() to remove it.
+
       cmd = "hostname -I | cut -d\' \' -f1"
       IP = subprocess.check_output(cmd, shell = True).decode('UTF-8')
 
-      cmd = "top -bn1 | grep load | awk '{printf \"C: %.2f\", $(NF-2)}'"
-      CPU = subprocess.check_output(cmd, shell = True).decode('UTF-8')
+      cmd = "top -bn1 | grep load | awk '{printf \"%.2f\", $(NF-2)}'"
+      CPU = subprocess.check_output(cmd, shell = True).decode('UTF-8').strip()
       
-      cmd = "free -m | awk 'NR==2{printf \"M: %s/%sMB\", $3,$2 }'"
-      MemUsage = subprocess.check_output(cmd, shell = True).decode('UTF-8')
+      cmd = "free -m | awk 'NR==2{printf \"%s / %sMB\", $3,$2 }'"
+      MemUsage = subprocess.check_output(cmd, shell = True).decode('UTF-8').strip()
       
-      cmd = "df -h | awk '$NF==\"/\"{printf \"D: %d/%dGB\", $3,$2}'"
-      Disk = subprocess.check_output(cmd, shell = True).decode('UTF-8')
+      cmd = "df -h | awk '$NF==\"/\"{printf \"%d / %dGB\", $3,$2}'"
+      Disk = subprocess.check_output(cmd, shell = True).decode('UTF-8').strip()
 
-      cmd = "vcgencmd measure_temp"
-      Temp = subprocess.check_output(cmd, shell = True).decode('UTF-8')
-      Temp = Temp.replace("temp=","")
-      # Display in F
-      Temp = (Temp * 9/5) + 32
+      # We just want the numerical portion of the temperature:
+      cmd = "vcgencmd measure_temp | egrep -o '[0-9]*\.[0-9]*'"
+      Temp = subprocess.check_output(cmd, shell = True).decode('UTF-8').strip()
+      # Round to nearest degree, display in F
+      Temp_float = round((float(Temp)) * 9/5 + 32, 1) 
+      Temp = str(Temp_float)
 
       # Display system stats
-      draw.text((x, top), "IP: " + IP),  font=font, fill=255)    
+      draw.text((x, top), "IP: " + IP,  font=font, fill=255)    
       draw.text((x, top+16), "CPU:  " + CPU, font=font, fill=255)
       draw.text((x, top+32), "Mem:  " + MemUsage, font=font, fill=255)
       draw.text((x, top+40), "Disk: " + Disk,font=font, fill=255)
-      draw.text((x, top+48), "Temp: " + Temp,font=font, fill=255)
+      draw.text((x, top+48), "Temp: " + Temp + "F",font=font, fill=255)
 
       mode=0
     
     # Display image.
     disp.image(image)
-    disp.display()
+    disp.show()
 
   counter += 1
   time.sleep(1)
